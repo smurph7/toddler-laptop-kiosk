e3ee0a3fccb7ce4e61617072d0d0da009a45079c
Sarah <sarahnmurphy93@gmail.com>
2026-05-14 01:51:03 +0100

    update exec

--- toddler-laptop-kiosk.sh
+++ toddler-laptop-kiosk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo -ne '\033c\033]0;Toddler Laptop Kiosk\a'
 base_path="$(dirname "$(realpath "$0")")"
-"$base_path/builds/toddler-laptop-kiosk.x86_64" "$@"
+"$base_path/toddler-laptop-kiosk.x86_64" "$@"
